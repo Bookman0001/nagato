c6887080cf4d3bc976c27eb1a7f1c0c0d5779ca2
Bookman0001 <odonel51486666@gmail.com>
2020-12-19 13:18:13 +0900

    modify tsconfig

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,19 @@ import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 import * as gtag from 'lib/gtag'
 
+interface Props {
+  Component: any
+  pageProps: any
+}
+
 //ブラウザ内部でルーターが切り替わった時に発火させている
-const App = ({ Component, pageProps }) => {
+const App = ({ Component, pageProps }: Props) => {
   const router = useRouter()
   useEffect(() => {
     if (!gtag.existsGaId) {
       return
     }
-    const handleRouteChange = (path) => {
+    const handleRouteChange = (path: string) => {
       gtag.pageview(path)
     }
     router.events.on('routeChangeComplete', handleRouteChange)
