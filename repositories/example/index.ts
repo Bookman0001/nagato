f6fb1493e995de095c27462f1cf031131d92ec62
Bookman0001 <odonel51486666@gmail.com>
2021-03-05 13:13:37 +0900

    sample

--- repositories/example/index.ts
+++ repositories/example/index.ts
@@ -1,21 +1,3 @@
-import { firebaseApplocation, region } from '@/config'
-
-export const existTodayWorkModelByUserId = async (
-  userId: string,
-  today: Date
-) => {
-  const FUNCTION_NAME = 'existDayWorkFromUserId'
-  const requestFromHosting = firebaseApplocation
-    .functions(region)
-    .httpsCallable(FUNCTION_NAME)
-  const requestingData = {
-    uid: userId,
-    workDay: today,
-  }
-
-  try {
-    return await requestFromHosting(requestingData)
-  } catch (e) {
-    return null
-  }
+export const sample = () => {
+  console.log('sample')
 }
