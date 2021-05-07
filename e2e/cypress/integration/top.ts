ea61702a262577c8ba9ac3fb0b8fdbb592c12a63
Bookman0001 <odonel51486666@gmail.com>
2021-05-07 21:01:58 +0900

    add cypress test

--- e2e/cypress/integration/top.ts
+++ e2e/cypress/integration/top.ts
@@ -1,4 +1,9 @@
-it('should correctly showing top page', () => {
+it('should correctly showing header', () => {
+  cy.visit('http://localhost:3000')
+  cy.get('header').contains('EN')
+})
+
+it('should correctly showing Introduction', () => {
   cy.visit('http://localhost:3000')
   cy.get('h1').contains('Introduction')
 })
