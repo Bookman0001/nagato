96d4b9610c43f0e18aa476b305be6ee4a1229b28
Bookman0001 <odonel51486666@gmail.com>
2021-06-22 00:22:13 +0900

    modify hooks

--- src/hooks/search/articles/index.ts
+++ src/hooks/search/articles/index.ts
@@ -4,11 +4,13 @@ import { SearchParams } from 'src/types'
 
 export function useSearchedArticles(params: SearchParams) {
   const fetchSearchedArticles = async () => {
-    await fetch('/api/articles', {
+    return await fetch(`${window.location.origin}/api/articles`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(params),
     })
   }
-  const { data, error } = useSWR('/api/articles', fetchSearchedArticles)
 
+  const { data, error } = useSWR('/api/articles', fetchSearchedArticles)
   return { data, error, isLoading: !data }
 }
