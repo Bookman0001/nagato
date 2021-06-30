50975e65c99d3025841d932bd891fb29a4a38ded
Bookman0001 <odonel51486666@gmail.com>
2021-06-30 23:42:42 +0900

    add e2e test

--- e2e/cypress/integration/top.ts
+++ e2e/cypress/integration/top.ts
@@ -1,9 +1,20 @@
-it('should correctly showing header', () => {
+beforeEach(() => {
   cy.visit('http://localhost:3000')
+})
+
+it('should correctly showing header', () => {
   cy.get('header').contains('EN')
 })
 
-it('should correctly showing Introduction', () => {
-  cy.visit('http://localhost:3000')
+it('should correctly showing section', () => {
   cy.get('h1').contains('Introduction')
+  cy.get('h2').contains('Oss/GitHub')
+  cy.get('h2').contains('Career')
+  cy.get('h2').contains('Articles')
+})
+
+it('should searched', () => {
+  cy.get('input').type('Next.js').type('{enter}')
+  cy.location('pathname').should('eq', '/posts')
+  cy.location('search').should('eq', '?keyword=Next.js')
 })
