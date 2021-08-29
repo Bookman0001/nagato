845b8a27902733ceae6266a53b203c26bd196271
Bookman0001 <odonel51486666@gmail.com>
2021-08-29 11:46:30 +0900

    modify package.json

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -3,16 +3,20 @@ import axios, { AxiosError } from 'axios'
 const X_API_BASE_URL: string = process.env.X_API_BASE_URL || ''
 const X_API_KEY: string = process.env.X_API_KEY || ''
 
-export async function get<T = any>(path: string) {
+axios.defaults.baseURL = X_API_BASE_URL
+axios.defaults.headers = {
+  'Content-type': 'application/json',
+  'X-API-KEY': X_API_KEY,
+}
+
+export async function get<T>(path: string) {
   const response = await axios
-    .get<T>(X_API_BASE_URL + path, {
-      headers: {
-        'Content-type': 'application/json',
-        'X-API-KEY': X_API_KEY,
-      },
+    .get<T>(path)
+    .then((response) => {
+      return response.data
     })
     .catch((error: AxiosError) => {
       throw error
     })
-  return response.data
+  return response
 }
