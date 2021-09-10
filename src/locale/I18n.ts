33e57c113017cd4e0879342f304f080167ceacef
Bookman0001 <odonel51486666@gmail.com>
2021-09-10 23:27:07 +0900

    remove unnecessary content

--- src/locale/I18n.ts
+++ src/locale/I18n.ts
@@ -9,15 +9,6 @@ const resources = {
       introduction:
         'フロントエンドをメインにエンジニアリングをしています。<0 />一貫性のあるユーザービリティの高いシステムを追求し続けています。',
       oss: '貢献しているOSS',
-      step1:
-        '医療システムパッケージウェアのうち画面と帳票部分の開発や改修を主に行っていました。<0 />また、SEという形でお客様との打ち合わせ、仕様策定など各種業務経験も培いました。',
-      step2:
-        'web企業のエンジニアとして転向、<0 />フルスタックとして多くのエンジニアリング経験を積みました。',
-      column21: '・SPAチャットアプリケーション作成',
-      column22: '・ECサイトの保守・CMSの導入など',
-      column23: '・某巨大サービスのフロント新規開発',
-      now: '現在は専門領域をフロントエンドとして日々開発に勤しんでいます。',
-      column31: '・自社webサービスの開発',
     },
   },
   en: {
@@ -26,15 +17,7 @@ const resources = {
       title: 'k-maker',
       introduction:
         'I am front-end developer.Especially, I love TypeScript and React component logic. Also, I experienced back-end enginnering including AWS management.',
-      step1: 'I developed and modified package software app(medical system). ',
       oss: 'contributed to oss as follows.',
-      step2:
-        'I developed web app using various tech tips in front-end and back-end',
-      column21: '・developing chat app',
-      column22: '・developing, supporing and monitoring e-commerce.',
-      column23: '・developing user servce in frontend.',
-      now: 'Now, majoring in front-end expert developer.',
-      column31: '・designing and developing in-house web service in front-end ',
     },
   },
 }
@@ -42,9 +25,7 @@ const resources = {
 i18n.use(initReactI18next).init({
   resources,
   lng: 'ja',
-
   keySeparator: false,
-
   interpolation: {
     escapeValue: false,
   },
