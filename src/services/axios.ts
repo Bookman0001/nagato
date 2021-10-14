bc15f61991e74d35b4aeb3c68745ababbe532f30
Bookman0001 <odonel51486666@gmail.com>
2021-10-14 21:20:14 +0900

    update axios 0.23.0

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -3,11 +3,10 @@ import axios, { AxiosError } from 'axios'
 const X_API_BASE_URL: string = process.env.X_API_BASE_URL || ''
 const X_API_KEY: string = process.env.X_API_KEY || ''
 
+// https://github.com/axios/axios
 axios.defaults.baseURL = X_API_BASE_URL
-axios.defaults.headers = {
-  'Content-type': 'application/json',
-  'X-API-KEY': X_API_KEY,
-}
+axios.defaults.headers.common['Content-type'] = 'application/json'
+axios.defaults.headers.common['X-API-KEY'] = X_API_KEY
 
 export async function get<T>(path: string) {
   const response = await axios
