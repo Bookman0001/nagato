80fceaf8813ab87682b4e8094d9d358cbafcecea
Bookman0001 <odonel51486666@gmail.com>
2021-11-08 21:31:33 +0900

    modify e2e

--- e2e/cypress/integration/top.ts
+++ e2e/cypress/integration/top.ts
@@ -1,18 +1,18 @@
-beforeEach(() => {
-  cy.visit('http://localhost:3000')
-})
+describe('top', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
 
-it('should correctly showing header', () => {
-  cy.get('header').contains('JA')
-})
+  it('should correctly showing header', () => {
+    cy.get('header').contains('JA')
+  })
 
-it('should correctly showing section', () => {
-  cy.get('h1').contains('Introduction')
-  cy.get('h2').contains('Oss/GitHub')
-  cy.get('h2').contains('Articles')
-})
+  it('should correctly showing section', () => {
+    cy.get('h2').contains('Articles')
+  })
 
-it('should correctly showing search more button', () => {
-  cy.get('button').click()
-  cy.location('pathname').should('eq', '/posts')
+  it('should correctly showing search more button', () => {
+    cy.get('button').click()
+    cy.location('pathname').should('eq', '/posts')
+  })
 })
