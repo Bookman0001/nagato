22c8a6e5670198ca47b064c1ea33b8a93bcd68f6
Bookman0001 <odonel51486666@gmail.com>
2021-11-28 20:10:08 +0900

    modify top

--- src/components/organisms/introduction/index.tsx
+++ src/components/organisms/introduction/index.tsx
@@ -1,39 +1,22 @@
 import styled from 'styled-components'
-import { Trans } from 'react-i18next'
 
 import MainTitle from 'src/components/atoms/mainTitle'
-import ParagraphContent from 'src/components/atoms/paragraphContent'
 import 'src/locale/I18n'
 
 export default function Introduction() {
   return (
     <Section>
       <TitleContainer>
-        <MainTitle>Introduction</MainTitle>
+        <MainTitle>My Blog</MainTitle>
       </TitleContainer>
-      <IntroductionWrapper>
-        <ParagraphContent>
-          <Trans i18nKey={'introduction'} components={[<br key={0} />]} />
-        </ParagraphContent>
-      </IntroductionWrapper>
     </Section>
   )
 }
 
-const Section = styled.section`
-  margin-bottom: 8rem;
-`
+const Section = styled.section``
 
 const TitleContainer = styled.div`
   h1 {
     align-items: baseline;
   }
-`
-
-const IntroductionWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  text-align: center;
-  align-items: center;
-  font-size: 1rem;
 `
