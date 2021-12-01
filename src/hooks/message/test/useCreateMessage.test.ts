0ba083ab3cdcb94dfac9faaa47c7831d37d9ac55
Bookman0001 <odonel51486666@gmail.com>
2021-12-01 22:27:59 +0900

    add test

---
+++ src/hooks/message/test/useCreateMessage.test.ts
@@ -0,0 +1,51 @@
+/**
+ * @jest-environment jsdom
+ */
+import { renderHook, act } from '@testing-library/react-hooks'
+import { waitFor } from '@testing-library/react'
+
+import { useCreateMessage } from 'src/hooks/message'
+import * as postMessage from 'src/repositories/client/message'
+
+jest
+  .spyOn(postMessage, 'postMessage')
+  .mockImplementationOnce(() => {
+    return Promise.resolve()
+  })
+  .mockImplementationOnce(() => {
+    return Promise.reject(new TypeError('something error happened'))
+  })
+
+describe('useCreateMessage', () => {
+  it('to be submitted with true status', async () => {
+    await act(async () => {
+      const { result } = renderHook(() => useCreateMessage())
+      await waitFor(async () => {
+        const success = await result.current.createMessage({
+          email: '',
+          name: '',
+          content: 'test message',
+        })
+        expect(result.current.error).toBe(null)
+        expect(result.current.isLoading).toBe(false)
+        expect(success).toBe(true)
+      })
+    })
+  })
+
+  it('to be submitted with false status', async () => {
+    await act(async () => {
+      const { result } = renderHook(() => useCreateMessage())
+      await waitFor(async () => {
+        const success = await result.current.createMessage({
+          email: '',
+          name: '',
+          content: 'test message',
+        })
+        expect(result.current.error).not.toBe(null)
+        expect(result.current.isLoading).toBe(false)
+        expect(success).toBe(false)
+      })
+    })
+  })
+})
---
+++ src/util/api/test/index.test.ts
@@ -0,0 +1,17 @@
+import { validateFormParams } from 'src/util/api'
+
+describe('validateFormParams', () => {
+  it('should return false with undefined data', () => {
+    expect(validateFormParams(undefined)).toBe(false)
+  })
+
+  it('should return true with  data', () => {
+    expect(
+      validateFormParams({
+        email: 'example@example.com',
+        name: 'name',
+        content: 'content',
+      })
+    ).toBe(true)
+  })
+})
