1dde4eba9b1d34e9d635b6f61650b81c4691d1ce
Bookman0001 <odonel51486666@gmail.com>
2023-03-25 13:09:51 +0900

    renovate node to v18

--- .storybook/webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path')
-
-module.exports = ({ config }) => {
-  config.resolve.alias = {
-    ...config.resolve.alias,
-    src: path.resolve(__dirname, '../src'),
-  }
-  return config
-}
