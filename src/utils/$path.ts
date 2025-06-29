e5e9884b6c42d37fbf6314c579191647564778f1
k_n <35423129+Bookman0001@users.noreply.github.com>
2025-06-29 10:16:29 +0900

    chore: eslint-update-v9 (#3468)

--- src/utils/$path.ts
+++ src/utils/$path.ts
@@ -1,19 +1,19 @@
 import type { OptionalQuery as OptionalQuery_o4op3q } from '../pages/posts';
 
 export const pagesPath = {
-  "posts": {
+  'posts': {
     _id: (id: string | number) => ({
       $url: (url?: { hash?: string | undefined } | undefined) => ({ pathname: '/posts/[id]' as const, query: { id }, hash: url?.hash })
     }),
     $url: (url?: { query?: OptionalQuery_o4op3q | undefined, hash?: string | undefined } | undefined) => ({ pathname: '/posts' as const, query: url?.query, hash: url?.hash })
   },
-  "preview": {
+  'preview': {
     $url: (url?: { hash?: string | undefined } | undefined) => ({ pathname: '/preview' as const, hash: url?.hash })
   },
-  "reception": {
+  'reception': {
     $url: (url?: { hash?: string | undefined } | undefined) => ({ pathname: '/reception' as const, hash: url?.hash })
   },
-  "thanks": {
+  'thanks': {
     $url: (url?: { hash?: string | undefined } | undefined) => ({ pathname: '/thanks' as const, hash: url?.hash })
   },
   $url: (url?: { hash?: string | undefined } | undefined) => ({ pathname: '/' as const, hash: url?.hash })
