9be3b8ff76cc5c73eea160318a6d1d4fcae06675
Bookman0001 <odonel51486666@gmail.com>
2026-04-04 13:32:56 +0900

    fix: removal of axios

--- src/services/axios/axios.ts
+++ src/services/axios/axios.ts
@@ -1,32 +1,35 @@
-import axios, { AxiosError } from 'axios'
-
 const X_API_BASE_URL = process.env.X_API_BASE_URL ?? ''
 const X_API_KEY = process.env.X_API_KEY ?? ''
 
-axios.defaults.baseURL = X_API_BASE_URL
-axios.defaults.headers.common['Content-type'] = 'application/json'
-axios.defaults.headers.common['X-MICROCMS-API-KEY'] = X_API_KEY
+const headers = {
+  'Content-type': 'application/json',
+  'X-MICROCMS-API-KEY': X_API_KEY,
+}
 
 export async function get<T>(path: string) {
-  const response = await axios.get<T>(path).then((response) => {
-    return response.data
+  const response = await fetch(`${X_API_BASE_URL}${path}`, {
+    method: 'GET',
+    headers,
+  }).then((response) => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`)
+    }
+    return response.json() as Promise<T>
   })
   return response
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export async function post<T>(path: string, data: any) {
-  const response = await axios.post<T>(path, data).then((response) => {
-    return response.data
+  const response = await fetch(path, {
+    method: 'POST',
+    headers,
+    body: JSON.stringify(data),
+  }).then((response) => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`)
+    }
+    return response.json() as Promise<T>
   })
   return response
 }
-
-axios.interceptors.response.use(
-  function (response) {
-    return response
-  },
-  function (error: AxiosError) {
-    throw error
-  }
-)
