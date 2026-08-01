96961f3c83fe8be0416e6b5c7caa426bf88bfccc
Bookman0001 <odonel51486666@gmail.com>
2026-08-01 16:40:25 +0900

    fix it

--- src/components/organisms/header/Header.test.tsx
+++ src/components/organisms/header/Header.test.tsx
@@ -5,7 +5,6 @@ import { Header } from 'src/components/organisms/header'
 describe('Header', () => {
   it('should be rendered correctlly in default', () => {
     render(<Header />)
-    expect(screen.getByTestId('header-icon')).toBeInTheDocument()
     expect(screen.getByLabelText('TOPページに戻る')).toBeInTheDocument()
   })
 })
